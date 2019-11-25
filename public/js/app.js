c65f4204008e0db290c4fc762298ea6fc40a2ded
dayadam <dayadamuga@gmail.com>
2019-11-24 21:39:58 -0500

    find one, added get elements to html.l, place order functionality started

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@ function populateItemsOnPage(itemsJSONFromGetAllHTTP) {
     const stockQuantity = item.stock_quantity;
     const createdAt = item.createdAt;
     const updatedAt = item.updatedAt;
-    $(".shop-header").append(`
+    $(".items-row").prepend(`
     <div class="col-lg-4 col-md-6 mb-4">
     <div class="card h-100">
       <a href="#"
@@ -23,14 +23,20 @@ function populateItemsOnPage(itemsJSONFromGetAllHTTP) {
           src="http://placehold.it/700x400"
           alt=""
       /></a>
-      <div class="card-body">
+      <div class="card-body" data-id="${id}">
         <h4 class="card-title">
           <a href="#">${productName}</a>
         </h4>
         <h5>$${price}.00</h5>
-        <p class="card-text">
-          ${departmentName}
-        </p>
+        <p class="card-text">${departmentName}</p>
+        <p class="card-text">Stock Quantity: ${stockQuantity}</p>
+        <form>
+        <label for="order">Buy quantity:</label>
+        <input type="number" id="order" min="1" max="500" />
+        <button class="btn btn-primary place-order">
+          Place order
+        </button>
+        </form>
       </div>
       <div class="card-footer">
         <small class="text-muted"
@@ -40,9 +46,49 @@ function populateItemsOnPage(itemsJSONFromGetAllHTTP) {
     </div>
   </div>
     `);
+    $(`.card-body[data-id='${id}']`).data("product", item);
+    //.attr("data-test", `${stockQuantity}`);
+    //console.log($("div[data-id='3']").attr("data-test"));
+    //console.log($(`.card-body[data-id='${id}']`).data("product"));
   });
+  //return "ready for next event";
 }
 
+//type="submit"
+
+function getOneProduct(id) {
+  $.ajax({
+    url: `/api/products/${id}`,
+    method: "GET"
+  }).then(ans => console.log(ans));
+} // send along buying quat? nah, for the update one?? make an if statement with andserof this one, if quant ok then execute put request otherwise display insuff qunt
+
+function clickPlaceOrder(populateFinished) {
+  //$(".place-order").on("click", function(event) {
+  //event.preventDefault();
+  /*     getOneProduct(
+        $(".card-body")
+        .attr("data-id", "1")
+      //$(this)
+        .parent()
+        .parent()
+        .data("product").id
+    ); */
+  /*     console.log($(".card-body")
+    .attr("data-id", "1")
+  //$(this)
+    .parent()
+    .parent()
+    .data("product").id); */
+  //});
+  //console.log($("div[data-id='3']").attr("data-test"));
+  //console.log($("div[data-id='10']").attr("data-test"));
+}
+
+function placeOrder(stockQuantity, orderQuantity) {}
+
 $(document).ready(function() {
-  getAllItems().then(populateItemsOnPage);
+  getAllItems()
+    .then(populateItemsOnPage)
+    .then(clickPlaceOrder);
 });
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,7 +3,6 @@ var db = require("../models");
 // Routes
 // =============================================================
 module.exports = function(app) {
-
   // GET route for getting all of the posts
   app.get("/api/products", function(req, res) {
     db.Product.findAll({}).then(function(ans) {
@@ -11,4 +10,27 @@ module.exports = function(app) {
     });
   });
 
+  app.get("/api/products/:id", function(req, res) {
+    db.Product.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(ans) {
+      res.json(ans);
+    });
+  });
+
+  app.put("/api/products", function(req, res) {
+    db.Product.update(
+      {
+        stock_quantity: req.body.stock_quantity - req.body.purchase_quantity,
+        updatedAt: moment().format() //"YYYY-MM-DD HH:mm:ss"
+      },
+      {
+        where: {
+          id: req.body.id
+        }
+      }
+    );
+  });
 };
